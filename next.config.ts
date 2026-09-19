bfc377db51bbdb6ba83fdb160c9a29f6799adc91
07akashprasad <07akashprasad@users.noreply.github.com>
2026-09-19 23:14:27 +0530

    Configure Next.js static export for hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName = "Kosh-studios";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
